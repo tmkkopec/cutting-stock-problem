961e8d7f6c5da40725580333489a6886870519b3
skalawi <skalawiktor@gmail.com>
2017-05-14 16:28:31 +0200

    Added fixing grid after picture insertion

--- src/main/java/data/Canvas.java
+++ src/main/java/data/Canvas.java
@@ -14,6 +14,7 @@ public class Canvas {
     private int pictureX;
     private int pictureY;
     private List<Picture> pictures;
+    private BlankSpace startSpace;
 
     public Canvas(int width, int height){
         spaceRows = new LinkedList<>();
@@ -65,12 +66,94 @@ public class Canvas {
     }
 
     private void fixGrid(int width, int height) {
+        BlankSpace currentSpace;
+        BlankSpace rowSpace;
+        BlankSpace colSpace;
+        int startrow = 0;
+        int startcol = 0;
+        rowSpace = colSpace = currentSpace = spaceRows.get(0);
+        for (int i = 0; i < rows; i++){
+            if(currentSpace.getyStart() == pictureY) {
+                startrow = i;
+                rowSpace = currentSpace;
+                break;
+            }
+            currentSpace = currentSpace.getDownSpace();
+        }
+        currentSpace = spaceCols.get(0);
+        for (int i = 0; i < cols; i++){
+            if(currentSpace.getxStart() == pictureX) {
+                startcol = i;
+                colSpace = currentSpace;
+                break;
+            }
+            currentSpace = currentSpace.getRightSpace();
+        }
+        int endrow = startrow;
+        for (int i = startrow; i < rows; i++){
+            if(pictureY + height - 1 > rowSpace.getyEnd()){
+                endrow++;
+                rowSpace = rowSpace.getDownSpace();
+            }
+        }
+        int endcol = startcol;
+        for (int i = startcol; i < cols; i++){
+            if(pictureX + width - 1 > colSpace.getxEnd()){
+                endcol++;
+                colSpace = colSpace.getRightSpace();
+            }
+        }
+        for(int i = rows - 1; i >= 0; i--){
+            currentSpace = getSpace(i, endcol);
+            BlankSpace newSpace = new BlankSpace(pictureX + width, currentSpace.getxEnd(), currentSpace.getyStart(), currentSpace.getyEnd());
+            newSpace.setAvailable(currentSpace.isAvailable());
+            newSpace.setRightSpace(currentSpace.getRightSpace());
+            if(currentSpace.getDownSpace() != null) newSpace.setDownSpace(currentSpace.getDownSpace().getRightSpace());
+            else newSpace.setDownSpace(null);
 
+            currentSpace.setxEnd(pictureX + width -1);
+            currentSpace.setRightSpace(newSpace);
+        }
+        cols++;
+        for(int i = cols - 1; i >= 0; i--){
+            currentSpace = getSpace(endrow, i);
+            BlankSpace newSpace = new BlankSpace(currentSpace.getxStart(),currentSpace.getxEnd(),pictureY + height, currentSpace.getyEnd());
+            newSpace.setAvailable(currentSpace.isAvailable());
+            newSpace.setDownSpace(currentSpace.getDownSpace());
+            if(currentSpace.getRightSpace() != null) newSpace.setRightSpace(currentSpace.getRightSpace().getDownSpace());
+            else newSpace.setRightSpace(null);
+
+            currentSpace.setyEnd(pictureY + height -1);
+            currentSpace.setDownSpace(newSpace);
+        }
+        rows++;
+        makeUnavailable(getSpace(startrow,startcol),endrow - startrow + 1, endcol - startcol + 1);
+
+    }
+
+    private void makeUnavailable(BlankSpace rowSpace, int rows, int cols){
+        BlankSpace colSpace;
+        for (int i = 0; i < rows; i++){
+            colSpace = rowSpace;
+            for(int y = 0; y < cols; y++) {
+                colSpace.setAvailable(false);
+                colSpace = colSpace.getRightSpace();
+            }
+            rowSpace = rowSpace.getDownSpace();
+        }
+    }
+
+    private BlankSpace getSpace(int row, int col){
+        BlankSpace currentSpace = spaceCols.get(0);
+        for(int i = 0; i < row; i++) currentSpace = currentSpace.getDownSpace();
+        for(int i = 0; i < col; i++) currentSpace = currentSpace.getRightSpace();
+        return currentSpace;
     }
 
     private boolean checkSpaceInRow(BlankSpace space, int width, int height){
         if(space == null) return false;
         else if(space.isAvailable()){
+            startSpace = space;
             pictureX = space.getxStart();
             pictureY = space.getyStart();
             if (checkRight(space, width)) return checkDown(space, height);
@@ -81,19 +164,26 @@ public class Canvas {
     private boolean checkSpaceInCol(BlankSpace space, int width, int height){
         if(space == null) return false;
         else if (space.isAvailable()){
+            startSpace = space;
             pictureX = space.getxStart();
             pictureY = space.getyStart();
             if (checkDown(space, height)) return checkRight(space, width);
         }
         return checkSpaceInCol(space.getDownSpace(),width,height);
     }
-    //TODO: Implement this methood
+
     private boolean checkRight(BlankSpace space, int width) {
-        return false;
+        if(space == null) return false;
+        if(!space.isAvailable()) return false;
+        if(space.getWidth()>= width) return true;
+        return checkRight(space.getRightSpace(),width - space.getWidth());
     }
-    //TODO: Implement this method
+
     private boolean checkDown(BlankSpace space, int height) {
-        return false;
+        if(space == null) return false;
+        if(!space.isAvailable()) return false;
+        if(space.getHeight() >= height) return true;
+        return checkDown(space.getDownSpace(),height - space.getHeight());
     }
 
 }
